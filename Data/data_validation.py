b5d9f7f0250882c782351e665247c4ebb1e7e013
YuliaKokorieva <Yuliya.Pastushenko@gmail.com>
2023-05-25 19:06:42 +0300

    data validation and README updated

--- Data/data_validation.py
+++ Data/data_validation.py
@@ -1,17 +1,22 @@
 import os
 import pandas as pd
 
-folder_path = 'rides/'
-
+folder_path = 'data-csv/raw-data/rides/'
 duration_threshold = 10
 distance_threshold = 10
 
 combined_csv = pd.concat([pd.read_csv(os.path.join(folder_path, f)) for f in os.listdir(folder_path) if f.endswith('.csv')])
 
-combined_csv = combined_csv.dropna()  
-combined_csv = combined_csv[combined_csv['Duration (sec.)'].astype(int) >= duration_threshold] 
-combined_csv = combined_csv[combined_csv['Covered distance (m)'].astype(int) >= distance_threshold]  
+combined_csv = combined_csv.dropna()
+
+combined_csv = combined_csv[combined_csv['Duration (sec.)'].astype(int) >= duration_threshold]
+combined_csv = combined_csv[combined_csv['Covered distance (m)'].astype(int) >= distance_threshold]
+
+bike_stations = pd.read_csv('data-csv/raw-data/Helsingin_ja_Espoon_kaupunkipyöräasemat_avoin.csv') 
+
+valid_ids = bike_stations['ID']
+
+combined_csv = combined_csv[combined_csv['Departure_station_id'].isin(valid_ids)]
+combined_csv = combined_csv[combined_csv['Return_station_id'].isin(valid_ids)]
 
 combined_csv.to_csv(os.path.join(folder_path, 'combined.csv'), index=False)
-
-
--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Bike Rides project
-**Data** concatenated and validated with [Python script](https://github.com/YuliaKokorieva/bikes-project/blob/master/Data/data_validation.py). For importing and manipulating data in the Azure SQL Database, Azure Data Studio tool has been used.
+**Data** concatenated and validated with [Python script](https://github.com/YuliaKokorieva/bikes-project/blob/master/Data/data_validation.py). 
+Validation included:
+1. removing the rides with missing values
+2. removing the rides that lasted less than 10 secs and less then 10 m
+3. removing the rides, where Departure or Return station is not among the stations in the list of Bike stations
+
+For importing and manipulating data in the Azure SQL Database, Azure Data Studio tool has been used.
+
 
 **Backend** hosted in Azure:
 
