fe8f8b671570513780cffa16c44db0eda4e07111
YuliaKokorieva <Yuliya.Pastushenko@gmail.com>
2023-07-10 17:34:53 +0300

    hardcoded baseUrls removed

--- BikeRidesFront/src/services/stationsservice.js
+++ BikeRidesFront/src/services/stationsservice.js
@@ -1,8 +1,6 @@
 import axios from 'axios'
 
-//const baseUrl = process.env.REACT_APP_FUNC_SOURCE === 'local' ? process.env.REACT_APP_LOCAL_BASE_URL : process.env.REACT_APP_BASE_URL
-
-const baseUrl = 'https://bikes-project-backend.azurewebsites.net'
+const baseUrl = process.env.REACT_APP_FUNC_SOURCE === 'local' ? process.env.REACT_APP_LOCAL_BASE_URL : process.env.REACT_APP_BASE_URL
 
 const getAllStations = async () => {
   try {
